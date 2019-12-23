b940b5630f8e182381862da29ed61a1759d0d95e
Anup Sukumaran <anup.sukumaran9@gmail.com>
2019-12-23 15:30:27 +0530

    - Dec 23, 2019

--- Tests/MyPopUpTests/MyPopUpTests.swift
+++ Tests/MyPopUpTests/MyPopUpTests.swift
@@ -6,7 +6,7 @@ final class MyPopUpTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(MyPopUp().text, "Hello, World!")
+       // XCTAssertEqual(MyPopUp.text, "Hello, World!")
     }
 
     static var allTests = [
